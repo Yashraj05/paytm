0336b55903c78bfbe1577dbb10e267a6bbdb41df
Surya <surya.s@scalacoders.com>
2023-11-23 13:06:54 +0530

    paytm

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ import express from 'express';
 import paymentInitiationRouter from './paymentInitiation.js';
 import paymentResponseRouter from './paymentResponse.js';
 import cors from 'cors';
-app.use(cors());
+
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
